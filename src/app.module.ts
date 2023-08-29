577dec52d735afe4ad32eb2ddb50f645ff4d009e
f-asadov <57989749+f-asadov@users.noreply.github.com>
2023-08-29 17:07:03 +0400

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
